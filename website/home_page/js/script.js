f364832f1f02b6331e9e434c13e026a70bfc85f5
Vaghela Manan <139101270+manveer24@users.noreply.github.com>
2023-12-23 11:11:48 +0530

    Add files via upload

---
+++ website/home_page/js/script.js
@@ -0,0 +1,44 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+  // captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Unmute the audio after a short delay (e.g., 1000 milliseconds or 1 second)
+  setTimeout(function () {
+    document.getElementById('backgroundAudio').muted = false;
+  }, 1000); // Specify the time to wait in milliseconds
+});
+
